e54141a86a52afd61d4849cd397b8b88458078d7
Shiqing <shiqing-thu18@yandex.com>
2019-05-02 14:25:53 +0800

    :turtle: Implement list retrieval

--- crawler/crawl.js
+++ crawler/crawl.js
@@ -6,7 +6,10 @@ const asyncGet = (url) => new Promise((resolve, reject) => {
     const contentType = res.headers['content-type'];
 
     let isJSON = false;
-    if (statusCode !== 200) {
+    if (statusCode === 302) {
+      const redirect = asyncGet(res.headers['location']);
+      return redirect.then(resolve).catch(reject);
+    } else if (statusCode !== 200) {
       return reject(new Error(`Unexpected status code: ${statusCode}`));
     } else if (/^application\/json/.test(contentType)) {
       isJSON = true;
@@ -39,8 +42,33 @@ const asyncTryGet = async (url, tries) => {
   return null;
 };
 
+const fetchList = async (list, limitArg) => {
+  const limit = 500;
+  const ret = [];
+  let continueParam = '';
+  while (1) {
+    const response = await asyncTryGet(`https://cavestory.fandom.com/api.php?format=json&action=query&list=${list}&${limitArg}=${limit}${continueParam}`);
+
+    const queryList = response['query'][list];
+    for (let i = 0; i < queryList.length; i++) ret.push(queryList[i]);
+
+    const queryContinue = response['query-continue'];
+    const continueObj = queryContinue ? queryContinue[list] : null;
+    if (!continueObj) break;
+    continueParam = '&';
+    for (let i in continueObj) continueParam += `${i}=${continueObj[i]}`;
+  }
+  return ret;
+};
+
 (async () => {
-  console.log(await asyncTryGet('https://cavestory.fandom.com/index.php?curid=2029&action=raw'));
-  console.log(await asyncTryGet('https://cavestory.fandom.com/api.php?format=json&action=query&list=allpages&aplimit=10'));
-  console.log(await asyncTryGet('https://cavestory.fandom.com/404'));
+  const pages = await fetchList('allpages', 'aplimit');
+  console.log(pages);
+  console.log(pages.length);
+  const categories = await fetchList('allcategories', 'aclimit');
+  console.log(categories);
+  console.log(categories.length);
+  const images = await fetchList('allimages', 'ailimit');
+  console.log(images);
+  console.log(images.length);
 })();
