ab23bbf165dd5196b9a98b0c11ea9b5141743f90
Shiqing <shiqing-thu18@yandex.com>
2019-05-02 23:28:26 +0800

    :cyclone: QuQ

--- painter/main.c
+++ painter/main.c
@@ -1,7 +1,39 @@
 #include "raylib.h"
 #include "global.h"
+#include <stdlib.h>
 
 #define REPO_TITLE "solid-octo-palm-tree"
+
+static const Vector2 Leaf[16] = {
+    {25, 25},
+    {100, 25},
+    {100, 100},
+    {25, 100}
+};
+static Vector2 *LeafScaled = NULL;
+
+Vector2 *GenerateAnchoredPoly(
+    const Vector2 *points, int numPoints,
+    Vector2 offset, Vector2 anchor, float scale)
+{
+    Vector2 *scaledPoints = (Vector2 *)malloc(sizeof(Vector2) * numPoints);
+    for (int i = 0; i < numPoints; i++) {
+        scaledPoints[i].x = offset.x + (points[i].x - anchor.x) * scale;
+        scaledPoints[i].y = offset.y + (points[i].y - anchor.y) * scale;
+    }
+    return scaledPoints;
+}
+
+void DrawIcon(Vector2 offset, float scale, double t)
+{
+    LeafScaled = GenerateAnchoredPoly(Leaf, 16, offset, (Vector2){0.5, 1}, 25);
+
+    DrawLineEx(
+        (Vector2){offset.x - 200 * scale, offset.y},
+        (Vector2){offset.x + 200 * scale, offset.y},
+        3, DARKGRAY);
+    DrawPolyExLines(Leaf, 4, DARKGRAY);
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +52,7 @@ int main(int argc, char *argv[])
 
         BeginDrawing();
             ClearBackground((Color){240, 255, 249});
+            DrawIcon((Vector2){SCR_W * 2 / 3, SCR_H / 3}, 1, GetTime());
             DrawTextEx(font, REPO_TITLE, pos, 64, 0, DARKGRAY);
         EndDrawing();
     }
