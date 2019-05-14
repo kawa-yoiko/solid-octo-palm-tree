27e05bc4bb6108cdc5cf9cb1b650d37b388baf56
Shiqing <shiqing-thu18@yandex.com>
2019-05-14 10:02:08 +0800

    :mag: Enable MSAA

--- painter/global.c
+++ painter/global.c
@@ -4,12 +4,12 @@ Font font;
 
 void PalmTreeSetup()
 {
-    SetConfigFlags(FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
 
     InitWindow(SCR_W, SCR_H, "Visualizer");
     SetTargetFPS(60);
 
-    font = LoadFontEx("imprima.ttf", 64, 0, 0);
+    font = LoadFontEx("imprima.ttf", 128, 0, 0);
     GenTextureMipmaps(&font.texture);
     SetTextureFilter(font.texture, FILTER_POINT);
 }
