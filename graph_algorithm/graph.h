55741444175e2349030d372ba968d6f961bb46ca
bilibili <fenggaobuke@gmail.com>
2019-05-14 20:42:10 +0800

    implement pagerank

--- graph_algorithm/graph.h
+++ graph_algorithm/graph.h
@@ -13,7 +13,7 @@ struct Graph
 
 	std::vector<std::vector<unsigned>> tarjan();
 	std::vector<double> sssp(unsigned source);
-	std::vector<double> pagerank();
+	std::vector<double> pagerank(unsigned nIter, bool normalize = false);
 	std::vector<double> betweenness();
 	std::vector<double> closeness();
 };
---
+++ graph_algorithm/pagerank.cpp
@@ -0,0 +1,33 @@
+#include "graph.h"
+
+std::vector<double> Graph::pagerank(unsigned nIter, bool normalize)
+{
+	decltype(edge)& trans = normalize? *new decltype(edge)(edge): edge;
+	if (normalize)
+	{
+		auto trans = edge;
+		for (auto& l: trans)
+		{
+			double sum = 0;
+			for (auto const& p: l)
+				sum += p.w;
+			double normal = 1.0 / sum;
+			for (auto& p: l)
+				p.w *= normal;
+		}
+	}
+	unsigned N = edge.size();
+	std::vector<double> last(N, 1.0/N);
+	std::vector<double> next(N, 0);
+	while (nIter--)
+	{
+#pragma omp parallel for collapse(2)
+		for (unsigned i=0; i<N; ++i)
+			for (auto const& p: trans[i])
+				next[p.v] += p.w * last[i];
+		std::swap(last, next);
+	}
+	if (normalize) delete(&trans);
+	return next;
+}
+
