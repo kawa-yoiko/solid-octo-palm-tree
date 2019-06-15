2fc72c712e8111678523ae48ab6e7daa3828164a
Shiqing <shiqing-thu18@yandex.com>
2019-06-15 14:42:24 +0800

    :dizzy: Better integration

--- painter/graph_op.cc
+++ painter/graph_op.cc
@@ -17,14 +17,16 @@ static int x, y, hw, hh;
 
 struct GraphVertex {
     char *title;
-    float x, y, vx, vy;
+    float x, y, vx, vy, ax, ay, ax0, ay0;
     Color c;
     GraphVertex()
-      : title(nullptr), x(0), y(0), vx(0), vy(0), c({0, 0, 0, 0}) { }
+      : title(nullptr),
+        x(0), y(0), vx(0), vy(0), ax(0), ay(0), ax0(0), ay0(0),
+        c({0, 0, 0, 0}) { }
     ~GraphVertex() { if (title) free(title); }
 };
 
-static std::vector<GraphVertex> vertices;
+static std::vector<GraphVertex> vert;
 
 void InitGraph(int x, int y, int hw, int hh)
 {
@@ -32,7 +34,7 @@ void InitGraph(int x, int y, int hw, int hh)
     if (!f) return;
 
     g.edge.clear();
-    vertices.clear();
+    vert.clear();
     ::x = x;
     ::y = y;
     ::hw = hw;
@@ -40,7 +42,7 @@ void InitGraph(int x, int y, int hw, int hh)
 
     fscanf(f, "%d%d", &n, &m);
     g.edge.resize(n);
-    vertices.resize(n);
+    vert.resize(n);
 
     char s[1024];
     fgets(s, sizeof s, f);  // Ignore a newline
@@ -50,10 +52,10 @@ void InitGraph(int x, int y, int hw, int hh)
         int len = strlen(s);
         while (len > 0 && isspace(s[len - 1])) len--;
         s[len] = '\0';
-        vertices[i].title = strdup(s);
-        vertices[i].x = sin(M_PI * 2 * i / n) * 400;
-        vertices[i].y = cos(M_PI * 2 * i / n) * 400;
-        vertices[i].c = LIME_3;
+        vert[i].title = strdup(s);
+        vert[i].x = sin(M_PI * 2 * i / n) * 400;
+        vert[i].y = cos(M_PI * 2 * i / n) * 400;
+        vert[i].c = LIME_3;
     }
 
     for (int i = 0, u, v, w = 1; i < m; i++) {
@@ -68,34 +70,60 @@ void InitGraph(int x, int y, int hw, int hh)
 void VerletTick()
 {
     const float dt = 1.f / 60;
-    const float ALPHA = 1.f;
+    const float ALPHA = 1.f / 4;
+    const float BETA = 20.f;
+
+    // Integration (1)
+    for (int u = 0; u < n; u++) {
+        vert[u].x += (vert[u].vx + vert[u].ax / 2 * dt) * dt;
+        vert[u].y += (vert[u].vy + vert[u].ay / 2 * dt) * dt;
+        vert[u].ax0 = vert[u].ax;
+        vert[u].ay0 = vert[u].ay;
+        vert[u].ax = vert[u].ay = 0;
+    }
 
     // Link force
     for (int u = 0; u < n; u++)
         for (const auto &e : g.edge[u]) {
             int v = e.v;
-            float dx = (vertices[v].x + vertices[v].vx) - (vertices[u].x + vertices[u].vx);
-            float dy = (vertices[v].y + vertices[v].vy) - (vertices[u].y + vertices[u].vy);
+            float dx = (vert[v].x + vert[v].vx) - (vert[u].x + vert[u].vx);
+            float dy = (vert[v].y + vert[v].vy) - (vert[u].y + vert[u].vy);
             float l = sqrtf(dx * dx + dy * dy);
             if (fabsf(l) <= 1e-6) l = 1e-6;
-            //l = (l - 30) / l * ALPHA * 1;
-            l -= 180;
-            if (l <= 5) continue;
-            l = 1.0 / l;
-            dx *= l;
-            dy *= l;
+            float rate = (l - 90) / l;
+            dx *= rate * ALPHA;
+            dy *= rate * ALPHA;
             float b = 0.5;
-            vertices[v].vx -= dx * b;
-            vertices[v].vy -= dy * b;
-            vertices[u].vx += dx * (1 - b);
-            vertices[u].vy += dy * (1 - b);
+            vert[v].ax -= dx * b;
+            vert[v].ay -= dy * b;
+            vert[u].ax += dx * (1 - b);
+            vert[u].ay += dy * (1 - b);
         }
 
     // Repulsive force
+    for (int u = 0; u < n; u++)
+        for (int v = 0; v < n; v++) {
+            float dx = (vert[v].x + vert[v].vx) - (vert[u].x + vert[u].vx);
+            float dy = (vert[v].y + vert[v].vy) - (vert[u].y + vert[u].vy);
+            float dsq = dx * dx + dy * dy;
+            float d32 = dsq * sqrtf(dsq);
+            if (fabsf(d32) <= 1) d32 = 1;
+            dx *= BETA / d32;
+            dy *= BETA / d32;
+            vert[u].ax -= dx;
+            vert[u].ay -= dy;
+            vert[v].ax += dx;
+            vert[v].ay += dy;
+        }
 
+    // Integration (2)
     for (int u = 0; u < n; u++) {
-        vertices[u].x += vertices[u].vx * dt;
-        vertices[u].y += vertices[u].vy * dt;
+        vert[u].vx += (vert[u].ax + vert[u].ax0) / 2 * dt;
+        vert[u].vy += (vert[u].ay + vert[u].ay0) / 2 * dt;
+    }
+
+    // Centre "force"
+    for (int u = 0; u < n; u++) {
     }
 }
 
@@ -106,13 +134,13 @@ void VerletDraw()
     DrawLineStripWithChromaBegin();
     for (int i = 0; i < n; i++) {
         Vector2 p = (Vector2){
-            (float)(x + vertices[i].x),
-            (float)(y + vertices[i].y)
+            (float)(x + vert[i].x),
+            (float)(y + vert[i].y)
         };
         for (const auto &e : g.edge[i]) {
             Vector2 q = (Vector2){
-                (float)(x + vertices[e.v].x),
-                (float)(y + vertices[e.v].y)
+                (float)(x + vert[e.v].x),
+                (float)(y + vert[e.v].y)
             };
             int dsq = (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y);
             if (dsq < hw * hw / 2) {
@@ -128,8 +156,8 @@ void VerletDraw()
     DrawCirclesBegin(5, 6);
     for (int i = 0; i < n; i++) {
         DrawCirclesAdd((Vector2){
-            (float)(x + vertices[i].x),
-            (float)(y + vertices[i].y)
+            (float)(x + vert[i].x),
+            (float)(y + vert[i].y)
         }, LIME_8);
     }
     DrawCirclesEnd();
