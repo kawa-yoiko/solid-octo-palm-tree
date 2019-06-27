4bf3fc0dbe4b3a37df6cdd06550b04ccfaf603ba
bilibili <fenggaobuke@gmail.com>
2019-06-27 14:24:18 +0800

    upd graph interface

--- graph_algorithm/graph.h
+++ graph_algorithm/graph.h
@@ -11,10 +11,10 @@ struct Graph
 	};
 	std::vector<std::vector<Edge>> edge;
 
-	std::vector<std::vector<unsigned>> tarjan();
-	std::vector<double> sssp(unsigned source);
-	std::vector<double> pagerank(unsigned nIter, bool normalize = false);
-	std::vector<double> betweenness();
-	std::vector<double> closeness();
+	std::vector<std::vector<unsigned>> tarjan() const;
+	std::vector<double> sssp(unsigned source) const;
+	std::vector<double> pagerank(unsigned nIter, bool normalize = false) const;
+	std::vector<double> betweenness() const;
+	std::vector<double> closeness() const;
 };
 
